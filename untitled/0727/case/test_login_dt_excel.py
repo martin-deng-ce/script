df9784a298d759d26ab6593b06722256747da07c
martin-deng-ce <1637754392@qq.com>
2020-08-01 18:19:05 +0800

    “脚本”

--- untitled/0727/case/test_login_dt_excel.py
+++ untitled/0727/case/test_login_dt_excel.py
@@ -3,35 +3,49 @@
 # @Author:martin
 # @File:test_login_dt_excel.py
 from interface.school import School
+from commen.Excel_demo import Excelutil
 import requests
 import unittest
 import ddt
 s = requests.session()
-data = [
-    {"user":"admin", "psw":"660B8D2D5359FF6F94F8D3345698F88C","expect": True},
-    {"user":"admin1","psw":"111111","expect": False},
-    {"user":"admin2","psw":"222222","expect": False},
-    {"user":"admin3","psw":"xxxxxx","expect": False},
-]
+
+filepath = "testdata.xlsx"
+sheetName = "Sheet1"
+# data= Excelutil(filepath,sheetName)
+d = Excelutil(filepath,sheetName)
+data = d.dict_data()
+print(data)
+
+# data = [
+#     {"user":"admin", "psw":"660B8D2D5359FF6F94F8D3345698F88C","expect": True},
+#     {"user":"admin1","psw":"111111","expect": False},
+#     {"user":"admin2","psw":"222222","expect": False},
+#     {"user":"admin3","psw":"xxxxxx","expect": False},
+# ]
 
 @ddt.ddt
 class Test1(unittest.TestCase):
     def setUp(self):
         self.l = School(s)
 
-    @ddt.ddt(*data)
+    @ddt.data(*data)
     def test_0(self,testdata):
-        print("本次测试数据:%s" % testdata)
+        print("本次测试数据:%s" %testdata)
         user = testdata["user"]
         psw = testdata["psw"]
-        res = self.l.login(self, user, psw)
+        result = self.l.login(user, psw).text
         #实际结果
-        result = is_self.l.login_success(res)
+        if "退出登录"  in  result:
+            i = True
+        else:
+            i = False
+
+
         #True的断言，实际结果= 期望结果
         expect = testdata["expect"]
-        self.assertTrue(result==expect)
+        self.assertEqual(i,expect)
 
-    def tearDown(self):
-        self.s.close()
+    # def tearDown(self):
+    #     self.s.close()
 if __name__ == '__main__':
     unittest.main()
--- untitled/0727/commen/__init__.py
+++ untitled/0727/commen/__init__.py
@@ -1,4 +1,27 @@
-# -*- coding:utf-8 -*-
+ # -*- coding:utf-8 -*-
 # @Time:2020/7/27 17:55
 # @Author:martin
 # @File:__init__.py.py
+
+# if __name__ == '__main__':
+#
+#     for a in range(1, 10):
+#         for b  in range(a, 10):
+#             print("%d * %d= %d"%(a, b, a*b ),end=" ")
+#         print("")
+#
+# if __name__ == '__main__':
+#     for a in range(1, 10):
+#         for b in range(1,a+1):
+#             print("%d *%d= %d"%(b, a, b*a),end=" ")
+#         print("")
+# n = [34, 99, 53, 2, 68, 23, 59, 29, 9]
+# n.sort()
+# print(n)
+# a = len(n)#
+# print(a)
+# for b in range(0, a-1):
+#     for c in range(0, a-b-1):
+#         if n[c+1] > n[c]:
+#             n[c],n[c+1] = n[c+1],n[c]
+#     print(n)
